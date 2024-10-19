a55a9794e4541a832d29360e17b5678618d8126a
Lucatonello <lucatonellow@gmail.com>
2024-10-19 15:56:56 -0300

    route to send job experience data

--- controllers/membersController.js
+++ controllers/membersController.js
@@ -3,7 +3,7 @@ const db = require('../db/pool');
 const membersController = {
     getUserData: async (req, res) => {
         let { id, type } = req.params;
-        type = type === 'user' ? 'users' : type === 'company' ? 'companies' : null;
+        type = formatType(type);
 
         if (!type) {
             return res.status(400).send('Invalid type');
@@ -11,7 +11,10 @@ const membersController = {
 
         try {
             const response = await db.query(`
-                SELECT * FROM ${type}
+                SELECT *, 
+                COALESCE(array_length(connectionids, 1), 0) AS connections_count,
+                COALESCE(array_length(followerids, 1), 0) AS followers_count
+                FROM ${type}
                 WHERE id = $1
             `, [id]);
 
@@ -20,7 +23,49 @@ const membersController = {
             console.error(err);
             res.status(500).send('Error receiving data');
         }
-    }
+    },
+    getMemberActivity: async (req, res) => {
+        let { id, type } = req.params;
+        //format id column name
+        type = type == 'user' ? 'authorid' : type == 'company' ? 'companyid' : null;
+
+        try {
+            const posts = await db.query(`
+                SELECT * FROM posts
+                WHERE ${type} = $1
+            `, [id]);
+            const comments = await db.query(`
+                SELECT * FROM comments
+                WHERE ${type} = $1
+            `, [id]);
+
+            res.json({ posts: posts.rows, comments: comments.rows });
+        } catch (err) {
+            console.error(err)
+            res.status(500).send('Error receiving activity data');
+        }
+    },
+    getUserExperience: async (req, res) => {
+        const id = req.params.id;
+
+        try {
+            const result = await db.query(`
+                SELECT * 
+                FROM experience
+                WHERE id = $1
+            `, [id]);
+
+            res.json(result.rows);
+        } catch (err) {
+            console.error(err)
+            res.status(500).send('Error receiving experience data');
+        }
+    },
 };
+
+function formatType(type) {
+    type = type == 'user' ? 'users' : type == 'company' ? 'companies' : null;
+    return type;
+}
 
 module.exports = { membersController };
--- routes/members.js
+++ routes/members.js
@@ -3,5 +3,7 @@ const membersRouter = express.Router();
 const { membersController } = require('../controllers/membersController'); 
 
 membersRouter.get('/:action(getUserData|getCompanyData)/:type/:id', membersController.getUserData);
+membersRouter.get('/getMemberActivity/:type/:id', membersController.getMemberActivity);
+membersRouter.get('/getUserExperience/:id', membersController.getUserExperience);
 
 module.exports = { membersRouter };
