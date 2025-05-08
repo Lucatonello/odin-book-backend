377a892fc466e5bea13cb97b241ee860f077470d
Luca <160508987+Lucatonello@users.noreply.github.com>
2025-05-08 20:44:55 -0300

    Update pool.js

--- db/pool.js
+++ db/pool.js
@@ -2,10 +2,6 @@ const { Pool } = require("pg");
 
 const connectionString = process.env.DATABASE_URL;
 
-console.log("Database URL (parsed):", connectionString);
-console.log("process.env:", process.env);
-
-
 module.exports = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false } 
